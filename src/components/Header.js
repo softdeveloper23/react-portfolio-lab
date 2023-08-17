eeadf34f3d000344b33f36a93b7435dd8c9f53a8
unknown <brannongarrett77@gmail.com>
2023-08-17 17:05:24 +0200

    Added bonus

--- src/components/Header.js
+++ src/components/Header.js
@@ -33,7 +33,35 @@ const socials = [
 ];
 
 const Header = () => {
+
+  // **Step 5 (bonus)**
+
+  const headerRef = useRef(null);
+  useEffect(() => {
+    let prevScrollPos = window.scrollY;
+
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+      const headerElement = headerRef.current;
+      if (!headerElement) {
+        return;
+      }
+      if (prevScrollPos > currentScrollPos) {
+        headerElement.style.transform = "translateY(0)";
+      } else {
+        headerElement.style.transform = "translateY(-200px)";
+      }
+      prevScrollPos = currentScrollPos;
+    }
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, []);
+
   const handleClick = (anchor) => () => {
+    console.log("clicked");
     const id = `${anchor}-section`;
     const element = document.getElementById(id);
     if (element) {
@@ -55,6 +83,7 @@ const Header = () => {
       transitionDuration=".3s"
       transitionTimingFunction="ease-in-out"
       backgroundColor="#18181b"
+      ref={headerRef}
     >
       <Box color="white" maxWidth="1280px" margin="0 auto">
         <HStack
@@ -65,19 +94,21 @@ const Header = () => {
         >
           <nav>
             {/* Add social media links based on the `socials` data */}
-            <HStack spacing={8}>
-              {socials.map((social) =>
-                <a href={social.url} key={social.url}>
-                  <FontAwesomeIcon icon={social.icon} size="2x" />
-                </a>
-              )}
+            <HStack>
+              {
+                socials.map((social, index) => <a href={social?.url} key={index}><FontAwesomeIcon icon={social.icon} size="2x" /></a>)
+              }
             </HStack>
           </nav>
           <nav>
             <HStack spacing={8}>
               {/* Add links to Projects and Contact me section */}
-              <a href="#projects-section" onClick={handleClick("projects")}>Projects</a>
-              <a href="#contactme-section" onClick={handleClick("contactme")}>Contact Me</a>
+              <a href="#projects" onClick={handleClick("projects")}>
+                Projects
+              </a>
+              <a href="#contactme" onClick={handleClick("contactme")}>
+                Contact Me
+              </a>
             </HStack>
           </nav>
         </HStack>
